e695b63ed624d94413f985967b4c53429c153818
bagermatt <125579340+bagermatt@users.noreply.github.com>
2023-12-04 22:12:53 +0300

    Класс Person

---
+++ Abstraction/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class Person
+    {
+        private Random rnd = new Random();
+        private int height;
+        private int age;
+        public int Height { get { return height; } set { height = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public Person()
+        {
+            height = rnd.Next(50, 250);
+            age = rnd.Next(0, 100);
+        }
+
+        public Person(int height, int age)
+        {
+            this.height = height;
+            this.age = age;
+        }
+    }
+}
