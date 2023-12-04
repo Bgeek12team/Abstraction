df696152054e3b44166c464175d13e9427c297bf
bagermatt <125579340+bagermatt@users.noreply.github.com>
2023-12-04 23:26:44 +0300

    Консолька

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -6,32 +6,6 @@ using System.Threading.Tasks;
 
 namespace Abstraction
 {
-    public class Person
-    {
-        private Random rnd = new Random();
-        private int height;
-        private int age;
-        public int Height { get { return height; } set { height = value; } }
-        public int Age { get { return age; } set { age = value; } }
-
-        public Person()
-        {
-            height = rnd.Next(50, 250);
-            age = rnd.Next(0, 100);
-        }
-
-        public Person(int height, int age)
-        {
-            this.height = height;
-            this.age = age;
-        }
-
-        public override string ToString()
-        {
-            return ("Height = " + height + ", Age = " + age + "; ");
-        }
-    }
-
     static class P
     {
         static void Main()
@@ -69,6 +43,32 @@ namespace Abstraction
             Console.WriteLine(politech);
             politech.Randomize();
             Console.WriteLine(politech);
+        }
+    }
+
+    public class Person
+    {
+        private Random rnd = new Random();
+        private int height;
+        private int age;
+        public int Height { get { return height; } set { height = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public Person()
+        {
+            height = rnd.Next(50, 250);
+            age = rnd.Next(0, 100);
+        }
+
+        public Person(int height, int age)
+        {
+            this.height = height;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return ("Height = " + height + ", Age = " + age + "; ");
         }
     }
 }
