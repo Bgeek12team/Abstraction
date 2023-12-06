f4305e306242622e7b6b7af341aa7050fc1dc02c
ghimik <bolit006.br0m@gmail.com>
2023-12-06 22:46:30 +0300

    перезагрузка

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,17 +17,35 @@ namespace Abstraction
             //    kalibr[i] = new Person();
             kalibr[0] = new Person(181, 18);
             kalibr[1] = new Person(181, 19);
-            kalibr[2] = new Person(173, 19);
+            kalibr[2] = new Person(173, 19);  
+            //ХУЙ
             kalibr[3] = new Person(177, 19);
             kalibr[4] = new Person(174, 19);
             kalibr[5] = new Person(181, 20);
             kalibr[6] = new Person(178, 19);
+
+            FullName[] fullNames = new FullName[4];
+            fullNames[0] = new FullName("Борздов", "Марк", "Саныч");
+            fullNames[1] = new FullName("Богданов", "Никита", "Несаныч"); 
+            fullNames[2] = new FullName("Соболев", "Ромчик", "ДЕНСЕР");
+            fullNames[3] = new FullName("Аванесян", "Григорий", "ХАЧ"); 
+           
+            UniSorter<FullName> politech2 = new UniSorter<FullName>(fullNames);
+
+            ISorter<FullName>.Comparator compareByLastName = ((n1, n2) => string.Compare(n1.lastName, n2.lastName, StringComparison.OrdinalIgnoreCase));
+            ISorter<FullName>.Comparator compareByFirstName = ((n1, n2) => string.Compare(n1.firstName, n2.firstName, StringComparison.OrdinalIgnoreCase));
+            ISorter<FullName>.Comparator compareByMiddleName = ((n1, n2) => string.Compare(n1.middleName, n2.middleName, StringComparison.OrdinalIgnoreCase));
+
 
             UniSorter<Person> politech = new UniSorter<Person>(kalibr);
             ISorter<Person>.Comparator compareByHeight
                 = new ISorter<Person>.Comparator((p1, p2) => p1.Height - p2.Height);
             ISorter<Person>.Comparator compareByAge
                 = new ISorter<Person>.Comparator((p1, p2) => p1.Age - p2.Age);
+
+            Console.WriteLine("Исходный:");
+            politech2.BubbleSort(compareByLastName);
+            Console.WriteLine(politech2);
 
             Console.WriteLine("Исходный:");
             Console.WriteLine(politech);
@@ -55,6 +74,8 @@ namespace Abstraction
             politech.FastSort(compareByHeight, compareByAge);
             Console.WriteLine(politech);
             politech.Randomize();
+
+            Console.WriteLine();
         }
     }
 
@@ -81,6 +102,43 @@ namespace Abstraction
         public override string ToString()
         {
             return ("Height = " + height + ", Age = " + age + "; ");
+        }
+    }
+    public class FullName
+    {
+        public string lastName { get; }
+        public string firstName { get; }
+        public string middleName { get; }
+
+        public FullName(string lastName, string firstName, string middleName)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.middleName = middleName;
+        }
+
+        public int CompareTo(FullName other)
+        {
+            int lastNameComparison = string.Compare(lastName, other?.lastName, StringComparison.OrdinalIgnoreCase);
+
+            if (lastNameComparison != 0)
+            {
+                return lastNameComparison;
+            }
+
+            int firstNameComparison = string.Compare(firstName, other?.firstName, StringComparison.OrdinalIgnoreCase);
+
+            if (firstNameComparison != 0)
+            {
+                return firstNameComparison;
+            }
+
+            return string.Compare(middleName, other?.middleName, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override string ToString()
+        {
+            return $"ФИО: {lastName} {firstName} {middleName},\n";
         }
     }
 }
