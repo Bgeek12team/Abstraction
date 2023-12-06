95fe25fa70d433668940505d2437c563f31061fb
ghimik <bolit006.br0m@gmail.com>
2023-12-06 23:02:08 +0300

    собрали робобота

--- consoleTest/Program.cs
+++ consoleTest/Program.cs
@@ -10,6 +10,31 @@ namespace Abstraction
     {
         static void Main()
         {
+
+            FullName[] fullNames = new FullName[5];
+            fullNames[0] = new FullName("Борздов", "Марк", "Александрович");
+            fullNames[1] = new FullName("Богданов", "Никита", "Несаныч");
+            fullNames[2] = new FullName("Соболев", "Роман", "Валерьевич");
+            fullNames[3] = new FullName("Аванесян", "Григорий", "Ромаевич");
+            fullNames[4] = new FullName("Иванов", "Алексей", "Михайлович");
+
+            UniSorter<FullName> politech2 = new UniSorter<FullName>(fullNames);
+
+            ISorter<FullName>.Comparator compareByLastName
+                = ((n1, n2) => string.Compare(n1.LastName, n2.LastName, StringComparison.OrdinalIgnoreCase));
+            ISorter<FullName>.Comparator compareByFirstName
+                = ((n1, n2) => string.Compare(n1.FirstName, n2.FirstName, StringComparison.OrdinalIgnoreCase));
+            ISorter<FullName>.Comparator compareByMiddleName
+                = ((n1, n2) => string.Compare(n1.MiddleName, n2.MiddleName, StringComparison.OrdinalIgnoreCase));
+            politech2.FastSort(compareByLastName,
+                compareByFirstName,
+                compareByMiddleName);
+
+
+            Console.WriteLine("Исходный:");
+            Console.WriteLine(politech2);
+
+
             int n = 4;
             Person[] kalibr = new Person[n];
             for (int i = 0; i < n; i++)
@@ -73,6 +98,45 @@ namespace Abstraction
         public override string ToString()
         {
             return ("Height = " + height + ", Age = " + age + "; ");
+        }
+    }
+
+
+    public class FullName
+    {
+        public string LastName { get; }
+        public string FirstName { get; }
+        public string MiddleName { get; }
+
+        public FullName(string lastName, string firstName, string middleName)
+        {
+            this.LastName = lastName;
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+        }
+
+        public int CompareTo(FullName other)
+        {
+            int lastNameComparison = string.Compare(LastName, other?.LastName, StringComparison.OrdinalIgnoreCase);
+
+            if (lastNameComparison != 0)
+            {
+                return lastNameComparison;
+            }
+
+            int firstNameComparison = string.Compare(FirstName, other?.FirstName, StringComparison.OrdinalIgnoreCase);
+
+            if (firstNameComparison != 0)
+            {
+                return firstNameComparison;
+            }
+
+            return string.Compare(MiddleName, other?.MiddleName, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override string ToString()
+        {
+            return $"ФИО: {LastName} {FirstName} {MiddleName},\n";
         }
     }
 }
