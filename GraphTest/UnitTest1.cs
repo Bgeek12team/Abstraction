c7a1f56b28e1699fab475f1be9279312c8af4696
affinePassion <dali_ban26.07@mail.ru>
2023-12-06 22:52:35 +0300

    testsort

--- GraphTest/UnitTest1.cs
+++ GraphTest/UnitTest1.cs
@@ -197,4 +197,81 @@ namespace GraphTests
 
         }
     }
+    /// <summary>
+    ///  ласс, провер€ющий работу методов сортировки
+    /// </summary>
+    [TestClass]
+    public class TestSorts
+    {
+        /// <summary>
+        /// “ест провер€ет работу пузырьковой сортировки
+        /// </summary>
+        [TestMethod]
+        public void TestBubble()
+        {
+            IList<double> nums = new List<double> { -1.9, -6, -2.5, -4.3 };
+            IList<double> nums1 = new List<double>() { -6, -4.3, -2.5, -1.9 };
+            UniSorter<double> ints = new UniSorter<double>(nums);
+            UniSorter<double> ints1 = new UniSorter<double>(nums1);
+            ISorter<double>.Comparator comparebymax = new ISorter<double>.Comparator((p1, p2) => (int)(p1 - p2));
+            ints.BubbleSort(comparebymax);
+            Assert.AreEqual(ints1.ToString(), ints.ToString());
+        }
+        /// <summary>
+        /// “ест провер€ет работу сортировки вставками
+        /// </summary>
+        [TestMethod]
+        public void TestInsert()
+        {
+            IList<int> nums = new List<int> { -6, 0, 2, 0, 0, -2, -4, 0, 5, 7, 2 };
+            IList<int> nums1 = new List<int>() { -6, -4, -2, 0, 0, 0, 0, 2, 2, 5, 7 };
+            UniSorter<int> ints = new UniSorter<int>(nums);
+            UniSorter<int> ints1 = new UniSorter<int>(nums1);
+            ISorter<int>.Comparator comparebymax = new ISorter<int>.Comparator((p1, p2) => p1 - p2);
+            ints.InsertSort(comparebymax);
+            Assert.AreEqual(ints1.ToString(), ints.ToString());
+        }
+        /// <summary>
+        /// “ест провер€ет работу сортировки Ўелла
+        /// </summary>
+        [TestMethod]
+        public void TestShell()
+        {
+            IList<int> nums = new List<int> { 218391, -3210321, 0, 23, 12932138, 321032193, -2313120 };
+            IList<int> nums1 = new List<int>() { -3210321, -2313120, 0, 23, 218391, 12932138, 321032193 };
+            UniSorter<int> ints = new UniSorter<int>(nums);
+            UniSorter<int> ints1 = new UniSorter<int>(nums1);
+            ISorter<int>.Comparator comparebymax = new ISorter<int>.Comparator((p1, p2) => p1 - p2);
+            ints.ShellSort(comparebymax);
+            Assert.AreEqual(ints1.ToString(), ints.ToString());
+        }
+        /// <summary>
+        /// “ест провер€ет работу быстрой сортировки
+        /// </summary>
+        [TestMethod]
+        public void TestFast()
+        {
+            IList<int> nums = new List<int> { -1111, 321321, -32932, 23132131 };
+            IList<int> nums1 = new List<int>() { -32932, -1111, 321321, 23132131 };
+            UniSorter<int> ints = new UniSorter<int>(nums);
+            UniSorter<int> ints1 = new UniSorter<int>(nums1);
+            ISorter<int>.Comparator comparebymax = new ISorter<int>.Comparator((p1, p2) => (p1 - p2));
+            ints.FastSort(comparebymax);
+            Assert.AreEqual(ints1.ToString(), ints.ToString());
+        }
+        /// <summary>
+        /// “ест провер€ет работу болотной-преболотной сортировки
+        /// </summary>
+        [TestMethod]
+        public void TestBogoBogoBogoBogo()
+        {
+            IList<int> nums = new List<int> { -19, -6, -25, -43 };
+            IList<int> nums1 = new List<int>() { -43, -25, -19, -6 };
+            UniSorter<int> ints = new UniSorter<int>(nums);
+            UniSorter<int> ints1 = new UniSorter<int>(nums1);
+            ISorter<int>.Comparator comparebymax = new ISorter<int>.Comparator((p1, p2) => p1 - p2);
+            ints.BogoBogoSort(comparebymax);
+            Assert.AreEqual(ints1.ToString(), ints.ToString());
+        }
+    }
 }
